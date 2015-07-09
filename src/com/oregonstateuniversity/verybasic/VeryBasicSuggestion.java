cb95661b771392a4b03b01108a0d3c51adf93665
lmast <coe_mast@engr.orst.edu>
2015-07-09 15:16:59 -0700

    gets a PsiElement and prints out its class and its toString() representation

--- src/com/oregonstateuniversity/verybasic/VeryBasicSuggestion.java
+++ src/com/oregonstateuniversity/verybasic/VeryBasicSuggestion.java
@@ -2,21 +2,43 @@ package com.oregonstateuniversity.verybasic;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.refactoring.RefactoringFactory;
+import com.intellij.refactoring.openapi.impl.JavaRefactoringFactoryImpl;
 
 /**
  * Created by lily on 7/9/15.
  */
 public class VeryBasicSuggestion extends AnAction {
+
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getData(PlatformDataKeys.PROJECT);
-        String txt = Messages.showInputDialog(project, "What is your name?",
-                "Input you name:", Messages.getQuestionIcon());
-        Messages.showMessageDialog(project, "Hello, " + txt +
-                        "!\nI am glad to see you.", "Information",
-                Messages.getInformationIcon());
+//        String txt = Messages.showInputDialog(project, "What is your name?",
+//                "Input you name:", Messages.getQuestionIcon());
+//        Messages.showMessageDialog(project, "Hello, " + txt +
+//                        "!\nI am glad to see you.", "Information",
+//                Messages.getInformationIcon());
+
+        PsiElement element = e.getData(LangDataKeys.PSI_ELEMENT);
+
+//        System.out.println(element.toString());
+
+        PsiElement parent = PsiTreeUtil.getParentOfType(element, PsiElement.class);
+
+//        System.out.println(parent.toString());
+
+        System.out.println(element.getClass());
+
+        RefactoringFactory factory = new JavaRefactoringFactoryImpl(project);
+//        factory = RefactoringFactory.getInstance(project);
+        factory.createRename(element, "somethingReallyLongSoWeWontMissIt");
+
+        System.out.println(element.toString());
 
     }
 }
