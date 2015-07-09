4ca89ecc40c959dfcfc4093f6a7f3d47b4fad584
lmast <coe_mast@engr.orst.edu>
2015-07-09 11:29:30 -0700

    popup asking for name works

--- src/com/oregonstateuniversity/verybasic/VeryBasicSuggestion.java
+++ src/com/oregonstateuniversity/verybasic/VeryBasicSuggestion.java
@@ -2,12 +2,21 @@ package com.oregonstateuniversity.verybasic;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 /**
  * Created by lily on 7/9/15.
  */
 public class VeryBasicSuggestion extends AnAction {
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        String txt = Messages.showInputDialog(project, "What is your name?",
+                "Input you name:", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt +
+                        "!\nI am glad to see you.", "Information",
+                Messages.getInformationIcon());
+
     }
 }
