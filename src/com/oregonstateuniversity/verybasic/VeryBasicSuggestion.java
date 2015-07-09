3e2f610453b7a7e9d1c8975709ba4c725c81f3f7
lmast <coe_mast@engr.orst.edu>
2015-07-09 10:19:38 -0700

    initial commit

---
+++ src/com/oregonstateuniversity/verybasic/VeryBasicSuggestion.java
@@ -0,0 +1,13 @@
+package com.oregonstateuniversity.verybasic;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by lily on 7/9/15.
+ */
+public class VeryBasicSuggestion extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
